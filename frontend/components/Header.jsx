002b488fdaca0862f9dca3c35879bf3ae9cfa05a
DebabrataBanik <debabratabanik02@gmail.com>
2026-04-17 14:03:14 +0530

    feat: add header UI

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,36 @@
+import { Bookmark, PlusIcon, Search, SearchIcon } from 'lucide-react'
+
+const Header = () => {
+  return (
+    <header >
+      <a href="/">
+        <div className='flex items-center gap-2 font-bold'>
+          <span className='inline-block p-1 bg-accent rounded-sm'>
+            <Bookmark size={15} className='text-bg-primary' />
+          </span>
+          Bookmark Manager
+        </div>
+      </a>
+
+      <div className='flex gap-2'>
+        <label className='relative flex items-center'>
+          <SearchIcon className='search-icon' aria-hidden='true' size={15} />
+          <input 
+            type="search"
+            name='search'
+            placeholder='Search by title...'
+            className='search-input'
+          />
+        </label>
+        <button className='add-btn' type='button'> 
+          <PlusIcon aria-hidden='true' size={15} /> Add Bookmark
+        </button>
+        <button type='button' className='user-btn'>
+          US
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
