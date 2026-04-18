7d7825d996c3d22f84ba998bd4197ab9a698486b
DebabrataBanik <debabratabanik02@gmail.com>
2026-04-18 23:00:03 +0530

    feat: setup mongoDB client with mongoose
    - fix lodash dep

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import { closeBrowserless } from './utils/browser.js'
 import { bookmarkRouter } from './routes/bookmark.js'
+import { connectDB } from './config/db.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -9,13 +13,15 @@ app.use(cors())
 
 app.use(express.json())
 
+await connectDB()
+
 app.use('/api', bookmarkRouter)
 
 app.all('/*splat', (req, res) => {
   res.status(404).send('Not Found')
 })
 
-app.listen(8000, () => console.log('server at http://localhost:8000'))
+app.listen(process.env.PORT, () => console.log(`server at http://localhost:${process.env.PORT}`))
 
 process.on('SIGINT', async () => { await closeBrowserless(); process.exit(0) })
 process.on('SIGTERM', async () => { await closeBrowserless(); process.exit(0) })
